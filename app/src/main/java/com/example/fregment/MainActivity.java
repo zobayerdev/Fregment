1020c8e533920bb99078c46b6734a9cea268534a
User <https://github.com/karno786/Sqlite_git_test>
2021-10-04 01:43:26 +0600

    BottomNavigation Added !!!

--- app/src/main/java/com/example/fregment/MainActivity.java
+++ app/src/main/java/com/example/fregment/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.fregment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+
+                    case R.id.nav_home:
+                        Toast.makeText(MainActivity.this, "Home Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_cart:
+                        Toast.makeText(MainActivity.this, "Cart Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_add:
+                        Toast.makeText(MainActivity.this, "Add Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_account:
+                        Toast.makeText(MainActivity.this, "Account Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return true;
+            }
+        });
     }
 }
